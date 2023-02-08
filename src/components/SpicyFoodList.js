e7189e0b990e4efff0ed9d8130c07a0dde9b67e3
JAMES <jameskibunja17@gmail.com>
2023-02-08 12:16:19 +0300

    Example Code

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,11 +6,25 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
+    // console.log(newFood);
   }
-
+function handleLiClick(id) {
+  const newFoodArray = foods.map((food) =>{
+   if(food.id === id){
+    return{
+      ...food,
+      heatLevel: food.heatLevel + 1,
+    };
+   }else {
+    return food;
+   }
+  });
+  setFoods(newFoodArray);
+}
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick ={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
@@ -22,5 +36,33 @@ function SpicyFoodList() {
     </div>
   );
 }
+
+
+
+// function handleFilterChange(event){
+//  const [filterBy, setFilterBy] = useState("All");
+//   setFilterBy(event.target.value);
+
+// return (
+  
+//   <select name= "filter" onChange ={handleFilterChange}>
+//         <option value="All">All</option>
+//     <option value="American">American</option>
+//     <option value="Sichuan">Sichuan</option>
+//     <option value="Thai">Thai</option>
+//     <option value="Mexican">Mexican</option>
+//   </select>
+// );
+// }
+
+
+// const foodsToDisplay = foods.filter((food) => {
+//   if (filterBy === "All") {
+//     return true;
+//   } else {
+//     return food.cuisine === filterBy;
+//   }
+// });
+
 
 export default SpicyFoodList;
